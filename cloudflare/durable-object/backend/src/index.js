3418e0ee5c4f3be0cc68a18856eae1695d9e8cc3
Aestheticsuraj234 <jhapriya458@gmail.com>
2025-11-07 00:54:51 +0530

    Realtime messaging with websockets

--- cloudflare/durable-object/backend/src/index.js
+++ cloudflare/durable-object/backend/src/index.js
@@ -1,8 +1,6 @@
-import { DurableObject } from "cloudflare:workers";
-
+import { DurableObject } from 'cloudflare:workers';
 
 export class Veet extends DurableObject {
-	
 	constructor(ctx, env) {
 		super(ctx, env);
 		this.sessions = new Map();
@@ -11,19 +9,54 @@ export class Veet extends DurableObject {
 		});
 	}
 
-	
-		async fetch(_req) {
+	async fetch(_req) {
 		const pair = new WebSocketPair();
 		this.ctx.acceptWebSocket(pair[1]);
 		this.sessions.set(pair[1], {});
 		return new Response(null, { status: 101, webSocket: pair[0] });
 	}
+
+	webSocketMessage(ws, msg) {
+		const session = this.sessions.get(ws);
+		if (!session.id) {
+			session.id = crypto.randomUUID();
+			ws.serializeAttachment({ ...ws.deserializeAttachment(), id: session.id });
+			ws.send(JSON.stringify({ ready: true, id: session.id }));
+		}
+		this.broadcast(ws, msg);
+	}
+	broadcast(sender, msg) {
+		const id = this.sessions.get(sender).id;
+		for (let [ws] of this.sessions) {
+			if (sender == ws) continue;
+			switch (typeof msg) {
+				case 'string':
+					ws.send(JSON.stringify({ ...JSON.parse(msg), id }));
+					break;
+				default:
+					ws.send(JSON.stringify({ ...msg, id }));
+					break;
+			}
+		}
+	}
+
+	close(ws) {
+		const session = this.sessions.get(ws);
+		if (!session?.id) return;
+		this.broadcast(ws, { type: 'left' });
+		this.sessions.delete(ws);
+	}
+	webSocketClose(ws) {
+		this.close(ws);
+	}
+	webSocketError(ws) {
+		this.close(ws);
+	}
 }
 
 export default {
-	
 	async fetch(request, env, _ctx) {
-			const upgrade = request.headers.get('Upgrade');
+		const upgrade = request.headers.get('Upgrade');
 		if (!upgrade || upgrade != 'websocket') {
 			return new Response('Expected upgrade to websocket', { status: 426 });
 		}
